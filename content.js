fd43db650f8e8f66de49e7c2ad8a9a3e0ff21607
Bootskiing <connortheguy12@gmail.com>
2020-09-06 17:51:01 -0400

    only runs on yummly. Also see's difference between recipe sources

--- content.js
+++ content.js
@@ -1,1 +1,21 @@
-alert('Eww, Yummly!');
+var source = document.getElementsByClassName("source-link font-bold micro-text greyscale-3")
+if (source[0].title == "YUMMLY"){
+	// redirect to #recipeDirections
+	url = location.replace(window.location.href + "#recipeDirections");
+
+} else {
+	// redirect to #directions
+	url = location.replace(window.location.href + "#directions");
+	
+}
+
+// Find all instances of "print"
+//const reg = new RegExp("print", "gi")
+//var source = document.getElementsByTagName("button");
+//var found = source.search(reg);
+
+// Follow Link
+
+
+// Disgusting
+//alert('Eww, Yummly!');
