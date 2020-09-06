64d6f565f1f897caa636f1909f9fa29d33818374
Bootskiing <connortheguy12@gmail.com>
2020-09-06 18:37:04 -0400

    doesn't really work

--- content.js
+++ content.js
@@ -1,21 +1,38 @@
 var source = document.getElementsByClassName("source-link font-bold micro-text greyscale-3")
 if (source[0].title == "YUMMLY"){
 	// redirect to #recipeDirections
-	url = location.replace(window.location.href + "#recipeDirections");
+	location.replace(window.location.href + "#recipeDirections");
 
 } else {
 	// redirect to #directions
-	url = location.replace(window.location.href + "#directions");
+	location.replace(window.location.href + "#directions");
 	
+	// Looking for print buttons
+	var buttons = document.getElementsByTagName("button");
+	for (let i = 0; i < buttons.length; i++){
+		if (buttons[i].text == "Print"){
+			// Every website does this differently, so this is just guessing
+			
+			if (buttons[i].hasAttribute("data-mv-print")){
+				location.replace(buttons[i].data-mv-print);
+			}
+		}
+	}
+	
+	// If that doesn't work, try looking for a's
+	var a = document.getElementsByTagName("a");
+	for (let i = 0; i < a.length; i++){
+		
+		if (a[i].hasAttribute("class")){
+			if (a[i].getAttribute("class").includes("print")){
+				// Chances are if there is a link, it's to the print page
+				if (a[i].hasAttribute("href")){
+					location.replace(a[i].href);
+				}
+			}
+		}
+	}
 }
-
-// Find all instances of "print"
-//const reg = new RegExp("print", "gi")
-//var source = document.getElementsByTagName("button");
-//var found = source.search(reg);
-
-// Follow Link
-
 
 // Disgusting
 //alert('Eww, Yummly!');
